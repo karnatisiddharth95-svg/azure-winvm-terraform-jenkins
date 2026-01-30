f9d708d647fd09dbeb65440978f8ffff4a994a77
karnatisiddharth95-svg <karnatisiddharth95@gmail.com>
2026-01-30 22:27:46 +0530

    Update variables.tf

--- az-ResourseGroup/variables.tf
+++ az-ResourseGroup/variables.tf
@@ -1,1 +1,17 @@
-vadcxc
+variable "rg_name" {
+  type = string
+  default = "rg-hamsi-terraform-lab"
+}
+
+variable "location" {
+  type = string
+  default = "West Europe"
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    "Environment" = "Development"
+    owner       = "hamsi"
+  }
+}
